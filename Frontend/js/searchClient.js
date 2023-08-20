30783052f70678b9d127d1c95de53c9829d137e3
AlenaYakovlevaV <137722996+AlenaYakovlevaV@users.noreply.github.com>
2023-08-20 11:11:22 +0300

    Update searchClient.js

--- Frontend/js/searchClient.js
+++ Frontend/js/searchClient.js
@@ -1,4 +1,4 @@
-import { searchResult } from "./clientsAPI.js";
+// import { searchResult } from "./clientsAPI.js";
 import { createClientItem } from "./createClientItem.js";
 
 // Поиск клиента
@@ -6,63 +6,47 @@ export const searchClients = (clients) => {
     const findList = document.querySelector('.find-list');
     const input = document.querySelector('.header__input');
 
-    clients.forEach(client => {
-        const findItem = document.createElement('li');
-        const findLink = document.createElement('a');
+    let fio;
+   
 
-        findItem.classList.add('find-list__item');
-        findLink.classList.add('find-list__link');
-
-        findLink.textContent = `${client.surname} ${client.name} ${client.lastName}`;
-        findLink.href = '#';
-
-        findItem.append(findLink);
-        findList.append(findItem);
-    });
-
-    // Перерисовка таблицы с данными найденных клиентов
-    const rewriteTable = async (str) => {
-        const response = await searchResult(str);
+    input.addEventListener('input', function search() {
+        findList.innerHTML = '';
         const tbody = document.querySelector('.clients__tbody');
         tbody.innerHTML = '';
-        for (const client of response) {
-            tbody.append(createClientItem(client));
-        }
-    }
+        for (const client of clients) {
+            const findItem = document.createElement('li');
+            const findLink = document.createElement('a');
+            findItem.classList.add('find-list__item');
+            findLink.classList.add('find-list__link');
 
-    // Поиск клиента по введенному значению
-    input.addEventListener('input', async () => {
-        const value = input.value.trim();
-        const foundItems = document.querySelectorAll('.find-list__link');
-
-        if (value !== '') {
-            rewriteTable(value);
-
-            foundItems.forEach(link => {
-                if (link.innerText.search(value) == -1) {
-                    link.classList.add('hide');
-                    link.innerHTML = link.innerText;
-                } else {
-                    link.classList.remove('hide');
-                    findList.classList.remove('hide');
-
-                    const str = link.innerText;
-                    link.innerHTML = insertMark(str, link.innerText.search(value), value.length);
+            fio = `${client.surname} ${client.name} ${client.lastName}`;
+            if (input.value !== '') {
+                findList.style.display = 'block';
+                if (fio.includes(input.value)) {
+                    tbody.append(createClientItem(client));    
+                    findLink.textContent = fio; 
+                    findLink.innerHTML = insertMark(findLink.innerText, findLink.innerText.search(input.value), input.value.length);
+                    findLink.addEventListener('click', () => {
+                        tbody.innerHTML = '';
+                        tbody.append(createClientItem(client));    
+                        findLink.textContent = fio; 
+                        findList.style.display = 'none';
+                        input.value = '';
+                    })
+                    findItem.append(findLink);
+                    findList.append(findItem);
                 }
-            })
-        } else {
-            foundItems.forEach(link => {
-                const tbody = document.querySelector('.clients__tbody');
-                tbody.innerHTML = '';
-
-                clients.forEach(client => tbody.append(createClientItem(client)));
-
-                link.classList.remove('hide');
-                findList.classList.add('hide');
-                link.innerHTML = link.innerText;
-            });
+            }  else {
+                tbody.append(createClientItem(client));
+            }
+            
         }
+        
     });
+
+    findList.addEventListener('mouseleave', () => {
+        findList.style.display = 'none';
+    })
     
     // Маркировка совпадений искомых и найденных данных
     const insertMark = (str, position, length) => str
